857b95ece36595902742da23a62ab052c306f29e
CMA <87302380@qq.com>
2019-04-29 13:43:27 +0200

    对截图代码进行重构，在截图时即可进行分割的处理。但目前截图代码不稳定 存在特殊情况

--- screenshot/bin/screenshot.py
+++ screenshot/bin/screenshot.py
@@ -3,40 +3,52 @@ from PIL import Image
 from io import BytesIO
 import time
 
-# options = Options()
-# options.headless = False
-
-
-
 #driver = webdriver.Chrome('..//chromedriver.exe')
 driver = webdriver.PhantomJS('..\\phantomjs-2.1.1-windows\\bin\\phantomjs.exe')
 #driver = webdriver.Firefox(options=options,executable_path=r'..\\geckodriver.exe')
 
 driver.get('https://item.jd.com/100002237037.html')
+#https://item.jd.com/100001155819.html 特殊情况 打算用爬虫去爬
 driver.maximize_window()
 
+detailDiv = driver.find_element_by_id('J-detail-content')
+detail = driver.find_element_by_class_name('ssd-module-wrap')
 
-detail = driver.find_element_by_id('J-detail-content')
-location = detail.location
 size = detail.size
-part = detail.size['height']//2400
+part = detail.size['height']//3000
 for i in range(0,part+1):
-    js = 'window.scrollTo(0,' + str(location['y'] + 2400*i)+ ')'
+    js = 'window.scrollTo(0,' + str(detail.location['y'] + 3000*i)+ ')'
     driver.execute_script(js)
     time.sleep(2)
 
 png = driver.find_element_by_tag_name("body").screenshot_as_png
 
-driver.close()
-
 im = Image.open(BytesIO(png))
-left = location['x']
-top = location['y']
-right = location['x'] + size['width']
-bottom = location['y'] + size['height']
 
+left = detail.location['x']
+top = detailDiv.location['y']
+right = detail.location['x'] + detail.size['width']
+bottom = detail.location['y'] + detail.size['height']
+
+driver.close()
 im = im.crop((left, top, right, bottom))
-im.save('../screenshot.png')
+
+def formatImage(image):
+    width = image.width
+    height = image.height
+    part = height//2400
+    for i in range(0,part):
+        path = '../image/screenshot-'+str(i)+'.png'
+        image.crop((0, 2400*i, width, 2400*(i+1))).save(path)
+
+    path = '../image/screenshot-' + str(part) + '.png'
+    image.crop((0, 2400*part, width, height)).save(path)
+
+if im.height < 2400:
+    im.save('../screenshot.png')
+else:
+    formatImage(im)
+
 
 
 
